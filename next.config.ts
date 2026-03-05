955b9b463a4593958aca90751016a709a730d249
Bernhard Wittmann <dev+github@bernhardwittmann.com>
2026-03-05 13:46:21 +0100

    ci: :construction_worker: add github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
